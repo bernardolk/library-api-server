13258fc8b77618d808d7cc094a7341d7b27396d8
bernardolk <bernardo.knackfuss@neoep.com.br>
2020-11-16 17:27:27 -0300

    cleanup

--- api-server/server.js
+++ api-server/server.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
-// const { schema } = require('./services/graphql');
 const { schema } = require('./api/graphql');
 
 const app = express();
 
 const client = process.env.CLIENT || false;
+const port = process.env.PORT || 4000;
 
 app.use('/graphql', graphqlHTTP({
   schema: schema,
   graphiql: client,
 }));
 
-app.listen(process.env.PORT || 4000, () => {
-  console.log('Running a GraphQL API server at localhost:4000/graphql');
+
+app.listen(port, () => {
+  console.log(`Running a GraphQL API server at localhost:${port}/graphql`);
 });
